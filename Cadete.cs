c8d45932624c85c701a19c84c157165f561165cb
leoardo4390 <tidus4390@gmail.com>
2025-08-24 19:11:21 -0300

    version final

--- Cadete.cs
+++ Cadete.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 class Cadete
 {
-    private static int idGlobal = 0;
+    private static int idCadete = 0;
 
     public int Id { get; private set; }
     public string Nombre { get; private set; }
@@ -14,10 +14,21 @@ class Cadete
 
     public Cadete(string nombre, string direccion, int telefono)
     {
-        Id = ++idGlobal;
+        Id = ++idCadete;
         Nombre = nombre;
         Direccion = direccion;
         Telefono = telefono;
         Pedidos = new List<Pedido>();
     }
+
+    public void AgregarPedido(Pedido pedido)
+    {
+        Pedidos.Add(pedido);
+    }
+
+    public void EliminarPedido(int nro)
+    {
+        Pedidos.RemoveAll(p => p.Nro == nro);
+    }
+    
 }
